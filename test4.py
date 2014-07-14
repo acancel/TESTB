dd7724bf9de251cfa571c33a0b60d2bc954a3401
alain <alain@medio-debian>
2014-07-14 18:40:58 +0200

    test2

--- test4.py
+++ test4.py
@@ -4,14 +4,36 @@
 import os
 import sys
 #import traceback
-import cv2 as cv
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 #from qrtools import QR
 
 #if __name__ == "__main__":
 #    device = 0
 #    decode = True
-capture = cv.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 for i in range(0, 10):
     ret, frame = capture.read()
-cv.imwrite("image.jpg", frame)
-    
+cv2.imwrite("image.jpg", frame)
+
+
+img = cv2.imread('image.jpg',0)
+img = cv2.medianBlur(img,5)
+
+ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+
+for i in xrange(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
